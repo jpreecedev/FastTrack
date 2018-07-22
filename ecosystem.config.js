681fc0d8d0c22769d80a28a66a53a98b3f3d8d8a
Jon Preece <jonpreece@hotmail.co.uk>
2018-07-22 07:20:40 +0100

    Fix ec2 instance path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-130-168-211.eu-west-2.compute.amazonaws.com',
+      host: 'ec2-35-177-32-193.eu-west-2.compute.amazonaws.com',
       key: '~/.ssh/fast-track.pem',
       ref: 'origin/master',
       repo: 'git@github.com:jpreecedev/fasttrack.git',
